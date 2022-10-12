59be7158ae4230a90a9d9384841ac7dea71f23df
calebtripp <calebtripp@outlook.com>
2022-10-12 15:44:47 -0400

    Exercise 1 Complete

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -9,27 +9,21 @@ namespace APIsAndJSON
         static void Main(string[] args)
         {
             var client = new HttpClient();
-
             var urlWest = "https://api.kanye.rest";
-            var westResponse = client.GetStringAsync(urlWest).Result;
-            var westQ = JObject.Parse(westResponse).GetValue("quote").ToString();
+            var urlSwanson = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
 
-            Console.WriteLine($"{westQ}\n");
+            for (int i = 0; i < 5; i++)
+            {
+                var westResponse = client.GetStringAsync(urlWest).Result;
+                var westQ = JObject.Parse(westResponse).GetValue("quote").ToString();
+                Console.WriteLine($"Kanye: {westQ}\n");
 
-            //var urlSwanson = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
-            //var swansonResponse = client.GetStringAsync(urlSwanson).Result;
-            //var swansonQ = JObject.Parse(swansonResponse).GetValue("quote").ToString();
+                var swansonResponse = client.GetStringAsync(urlSwanson).Result;
+                var swansonQ = JArray.Parse(swansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+                Console.WriteLine($"Ron: {swansonQ}\n");
 
-
-
-
-            //for (int i = 0; i < 5; i++)
-            //{
-            //    Console.WriteLine($"{westQ}\n");
-            //    Console.WriteLine($"{swansonQ}\n");
-            //}
-
-            // Use var  = JArray.Parse(swansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+                Console.WriteLine();
+            }
         }
     }
 }
