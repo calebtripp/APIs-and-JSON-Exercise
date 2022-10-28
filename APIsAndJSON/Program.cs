e5a3f2a00927fdbb1dd5d837dd838d2050892edf
calebtripp <calebtripp@outlook.com>
2022-10-28 12:00:11 -0400

    Refactored with Nate, needs work.

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -16,19 +16,20 @@ namespace APIsAndJSON
 
             for (int i = 0; i < 5; i++)
             {
+                //Kanye needed to be censored due to public image concerns
                 var westResponse = client.GetStringAsync(urlWest).Result;
                 var westQ = JObject.Parse(westResponse).GetValue("quote").ToString();
                 Console.Write($"Kanye: {westQ}\n");
-                Thread.Sleep(1000); 
+               Thread.Sleep(1000);
 
-                var norrisResponse = client.GetStringAsync(urlNorris).Result;
-                var norrisQ = JObject.Parse(norrisResponse).GetValue("value").ToString();
-                Console.Write($"Chuck Norris: {norrisQ}\n");
-                Thread.Sleep(1000);
+                //var norrisResponse = client.GetStringAsync(urlNorris).Result;
+                //var norrisQ = JObject.Parse(norrisResponse).GetValue("value").ToString();
+                //Console.Write($"Chuck Norris: {norrisQ}\n");
+               Thread.Sleep(1000);
 
-                var swansonResponse = client.GetStringAsync(urlSwanson).Result;              
-                Console.WriteLine($"Ron: {JArray.Parse(swansonResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim()}\n\n");
-                Thread.Sleep(1500);
+                var swansonQuotes = JArray.Parse(client.GetStringAsync(urlSwanson).Result);                       
+                Console.WriteLine($"Ron: {swansonQuotes[0]}");
+                Thread.Sleep(1500);                       
             }
         }
     }
