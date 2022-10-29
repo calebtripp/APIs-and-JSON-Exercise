d5e39d411a2a09a62095fd9df9d33ca37d2a4f78
calebtripp <calebtripp@outlook.com>
2022-10-29 11:23:13 -0400

    Method Refactor, West API works.

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -2,11 +2,14 @@
 using System.Runtime.Intrinsics.X86;
 using static System.Net.WebRequestMethods;
 using System.Threading;
+using System.Net;
 
 namespace APIsAndJSON
 {
+    // FAIR WARNING - Quotes are random, some are inappropriate.
     internal class Program
     {
+        public delegate string GetQuoteFromResponse(string response);
         static void Main(string[] args)
         {
             var client = new HttpClient();
@@ -14,23 +17,20 @@ namespace APIsAndJSON
             var urlSwanson = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
             var urlNorris = "https://api.chucknorris.io/jokes/random";
 
+            void PrintQuote(string urlCharacter, GetQuoteFromResponse f, string name)
+            {
+                var apiResponse = client.GetStringAsync(urlCharacter).Result;
+                var quote = f(apiResponse);
+                Console.Write($"{name}: {quote}\n");
+                Thread.Sleep(1000);
+            }
+
             for (int i = 0; i < 5; i++)
             {
-                //Kanye needed to be censored due to public image concerns
-                var westResponse = client.GetStringAsync(urlWest).Result;
-                var westQ = JObject.Parse(westResponse).GetValue("quote").ToString();
-                Console.Write($"Kanye: {westQ}\n");
-               Thread.Sleep(1000);
-
-                //var norrisResponse = client.GetStringAsync(urlNorris).Result;
-                //var norrisQ = JObject.Parse(norrisResponse).GetValue("value").ToString();
-                //Console.Write($"Chuck Norris: {norrisQ}\n");
-               Thread.Sleep(1000);
-
-                var swansonQuotes = JArray.Parse(client.GetStringAsync(urlSwanson).Result);                       
-                Console.WriteLine($"Ron: {swansonQuotes[0]}");
-                Thread.Sleep(1500);                       
+                PrintQuote(urlWest, r => JObject.Parse(r).GetValue("quote").ToString(), "West");
+                Thread.Sleep(1000);                
             }
         }
     }
 }
+
